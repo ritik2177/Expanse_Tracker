3fdf712dbbf6edb04de9c831f575710991c8b4c5
Ritik kumar <ritikkatsa2005@gmail.com>
2025-04-11 01:18:27 +0530

    update

--- script.js
+++ script.js
@@ -3,32 +3,57 @@ document.addEventListener('DOMContentLoaded', () => {
   const expenseNameInput = document.getElementById("expense-name");
   const expenseAmountInput = document.getElementById("expense-amount");
   const expenseList = document.getElementById("expense-list");
-  const TotalAmount = document.getElementById("total-amount");
+  const totalAmountDisplay = document.getElementById("total-amount");
 
-  let expenses = [];
+  let expenses = JSON.parse(localStorage.getItem('expenses')) || [];
   let totalAmount = calculateTotal();
 
+  renderExpenses();
+
   expenseForm.addEventListener('submit', (e) =>{
+    e.preventDefault(); 
+
     const name = expenseNameInput.value.trim();
     const amount = parseFloat(expenseAmountInput.value.trim());
 
-    if(name !== "" && isNaN(amount) && amount > 0){
+    if (name !== "" && !isNaN(amount) && amount > 0) {
       const newExpense = {
-        id: Date.new(),
+        id: Date.now(),
         name: name,
-        amount: amount
-      }
+        amount: amount,
+      };
       expenses.push(newExpense);
       saveExpenseTolocal();
+      renderExpenses();
+            updateTotal();
+
+      //clear input
+      expenseNameInput.value = "";
+      expenseAmountInput.value = "";
     }
-    console.log(typeof expenseAmountInput.value.trim())
+    console.log(typeof expenseAmountInput.value.trim());
   });
 
-  function calculateTotal(){
+  function renderExpenses(){
+    expenseList.innerHTML = "";
+    expenses.forEach( expense => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+      ${expense.name} - $${expense.amount}
+      <button data-id="${expense.id}">Delete</button>`;
+      expenseList.appendChild(li);
+    });
+  }
 
+  function calculateTotal(){
+    return expenses.reduce((sum, expense) => sum + expense.amount,0)
   }
   function saveExpenseTolocal(){
     localStorage.setItem("expenses", JSON.stringify(expenses));
   }
 
+  function updateTotal(){
+    totalAmount = calculateTotal();
+    totalAmountDisplay.textContent = total.toFixed(2);
+  }
 });
